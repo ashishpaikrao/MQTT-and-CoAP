4647b4d7b7e147ef5be7c5b7ce1e7e1080082098
ashishpaikrao <ashishpaikrao@hotmail.com>
2020-06-24 04:37:56 +0100

    Add files via upload

---
+++ MQTT_server.py
@@ -0,0 +1,6 @@
+
+import paho.mqtt.publish as publish
+
+publish.single("CoreElectronics/test", "Hello", hostname="test.mosquitto.org")
+publish.single("CoreElectronics/topic", "World!", hostname="test.mosquitto.org")
+print("Done")
